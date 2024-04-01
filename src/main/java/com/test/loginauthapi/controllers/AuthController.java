f6762f9f0e9325e653efa95339759fbdde0eefdf
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-03-31 21:26:36 -0300

    feat: create UserController and AuthController

---
+++ src/main/java/com/test/loginauthapi/controllers/AuthController.java
@@ -0,0 +1,55 @@
+package com.test.loginauthapi.controllers;
+
+import com.test.loginauthapi.domain.user.User;
+import com.test.loginauthapi.dto.LoginRequestDTO;
+import com.test.loginauthapi.dto.RegisterRequestDTO;
+import com.test.loginauthapi.dto.ResponseDTO;
+import com.test.loginauthapi.infra.security.TokenService;
+import com.test.loginauthapi.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository repository;
+    private final PasswordEncoder passwordEncoder;
+    private final TokenService tokenService;
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestBody LoginRequestDTO body) {
+        User user = this.repository.findByEmail(body.email()).orElseThrow(() -> new RuntimeException("User not found"));
+        if (passwordEncoder.matches(body.password(), user.getPassword())) {
+            String token = this.tokenService.generateToken(user);
+            return ResponseEntity.ok(new ResponseDTO(token, user.getName()));
+        }
+
+        return ResponseEntity.badRequest().build();
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody RegisterRequestDTO body) {
+        Optional<User> user = this.repository.findByEmail(body.email());
+
+        if (user.isEmpty()) {
+            User newUser = new User();
+            newUser.setPassword(passwordEncoder.encode(body.password()));
+            newUser.setEmail(body.email());
+            newUser.setName(body.name());
+            this.repository.save(newUser);
+
+            String token = this.tokenService.generateToken(newUser);
+            return ResponseEntity.ok(new ResponseDTO(token, newUser.getName()));
+        }
+
+
+        return ResponseEntity.badRequest().build();
+    }
+}
--- src/main/java/com/test/loginauthapi/infra/security/CustomUserDetailsService.java
+++ src/main/java/com/test/loginauthapi/infra/security/CustomUserDetailsService.java
@@ -7,11 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 
-@Service
+@Component
 public class CustomUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository repository;
